5ecba6d435ce9cf0c68516e4708dc6cea196640b
Zaki <bushirizaki@gmail.com>
2026-07-27 22:12:04 -0400

    added moving surfaces and remade jumpdown

--- kingdoms-in-ashes/Assets/Scripts/player_controller.gd
+++ kingdoms-in-ashes/Assets/Scripts/player_controller.gd
@@ -10,15 +10,23 @@ var direction = 0
 #const SPEED = 300.0
 #const JUMP_VELOCITY = -400.0
 
+func _input(event):
+	# Handle jump.
+	if event.is_action_pressed("jump") and is_on_floor():
+		velocity.y = jump_power * jump_multiplier
+	# Handle jump down
+	if event.is_action_pressed("move_down") and is_on_floor():
+		set_collision_mask_value(2, false)  # Disable Layer 2 collision
+		await get_tree().create_timer(0.2).timeout
+		set_collision_mask_value(2, true)   # Re-enable Layer 2 collision
+
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = jump_power * jump_multiplier
+
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
